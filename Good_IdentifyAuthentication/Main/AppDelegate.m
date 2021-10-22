58da7c74fd1c529795a3d3c5d1c1cfcc094f9bd2
hukai <hukaihope@gmail.com>
2021-10-22 11:18:28 +0800

    中煤集团更新

--- Good_IdentifyAuthentication/Main/AppDelegate.m
+++ Good_IdentifyAuthentication/Main/AppDelegate.m
@@ -109,7 +109,7 @@
     //更新公司信息
 //    [self requestSPinfo];
     //检查版本更新
-    [self checkVersion];
+//    [self checkVersion];
 //    [self checkNewVersion];
 //    [self checkNewVersion];
 //    [self checkUpdataWithPlist];
@@ -1994,7 +1994,7 @@
         YCLog(@"获取版本号失败！");
     }];
 }
-/*
+
 - (void)checkNewVersion{
     __weak typeof(self) weakSelf = self;
     AFHTTPSessionManager *manager  = [AFHTTPSessionManager manager];
@@ -2020,13 +2020,13 @@
 -(void)checkNewAppUpdate:(NSString *)appInfo updateURL:(NSString *)updateURL withFouce:(BOOL)fouce{
     //版本
     NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
-    CGFloat dispatchTime;
-    if ([self.window.rootViewController isKindOfClass: [TRUBaseTabBarController class]]) {
-        
-        dispatchTime = 0.1;
-    }else{
-        dispatchTime = 20;
-    }
+    CGFloat dispatchTime = 0.1;
+//    if ([self.window.rootViewController isKindOfClass: [TRUBaseTabBarController class]]) {
+//
+//        dispatchTime = 0.1;
+//    }else{
+//        dispatchTime = 20;
+//    }
     
     if ([self updeWithDicString:version andOldString:appInfo]) {
         if (fouce) {
@@ -2074,7 +2074,7 @@
     manager.requestSerializer =[AFHTTPRequestSerializer serializer];
     manager.responseSerializer.acceptableContentTypes =  [NSSet setWithObjects:@"text/html",@"text/plain",@"application/json",@"text/javascript",nil];
     NSString *baseUrl = [[NSUserDefaults standardUserDefaults] objectForKey:@"CIMSURL"];
-    NSString *updateUrl = [NSString stringWithFormat:@"%@/ios/cims.html",baseUrl];
+    NSString *updateUrl = [NSString stringWithFormat:@"%@/api/ios/cims.html",baseUrl];
     [manager GET:updateUrl parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         if ([responseObject isKindOfClass:[NSDictionary class]]) {
             NSString *url = responseObject[@"url"];
@@ -2140,7 +2140,7 @@
     return nil;
 }
 
-*/
+
 
 -(void)checkAppUpdate:(NSString *)appInfo{
     //版本
